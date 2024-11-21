9e576c87a71f86bd294fbaf724b8e7d289030c02
Doodflix <doodflixp@gmail.com>
2024-11-21 21:02:48 +0700

    Update piupiu.js

--- piupiu.js
+++ piupiu.js
@@ -2,7 +2,7 @@
         var puURL = 'http://google.com';
         var puTS = Math.round(+new Date()/1000);
         console.log('T.'+localStorage.puTS+'/'+puTS);
-        if (typeof localStorage.puTS == 'undefined' || parseInt(localStorage.puTS) <= (puTS - 3600)) {
+        if (typeof localStorage.puTS == 'undefined' || parseInt(localStorage.puTS) <= (puTS - 5)) {
             var links = document.getElementsByTagName('a');
             for(var i = 0, len = links.length; i < len; i++) {
                 links[i].onclick = function (e) {
