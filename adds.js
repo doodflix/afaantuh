6fb97660ee87ccede3564281b6f0d723a75149ca
Doodflix <doodflixp@gmail.com>
2024-11-21 21:31:39 +0700

    Update adds.js

--- adds.js
+++ adds.js
@@ -1,20 +1,12 @@
-function openLink() {
-            // Ambil waktu saat ini
-            const currentTime = new Date().getTime();
+const link = document.getElementById('myLink');
+        let canOpenPopup = true; // Flag untuk mengontrol pembukaan popunder
 
-            // Ambil waktu klik terakhir dari localStorage
-            const lastClickTime = localStorage.getItem(&#39;lastClickTime&#39;);
-
-            // Hitung waktu tunggu acak antara 30-50 detik
-            const randomWaitTime = Math.floor(Math.random() * (95 - 32 + 1)) + 50; // Dalam detik
-            const waitTimeInMillis = randomWaitTime * 1000;
-
-            // Jika belum pernah diklik atau sudah lebih dari waktu tunggu
-            if (lastClickTime === null || currentTime - lastClickTime &gt;= waitTimeInMillis) {
-                // Buka link di tab baru
-                window.open(&#39;https://google.com/&#39;, &#39;_blank&#39;);
-
-                // Simpan waktu klik saat ini ke localStorage
-                localStorage.setItem(&#39;lastClickTime&#39;, currentTime);
+        link.addEventListener('click', () => {
+            if (canOpenPopup) {
+                canOpenPopup = false; // Set flag menjadi false untuk mencegah pembukaan popunder berulang
+                setTimeout(() => {
+                    window.open('https://www.google.com', '_blank');
+                    canOpenPopup = true; // Set flag menjadi true setelah 30 detik
+                }, 30000); // 30000 milidetik = 30 detik
             }
-        }
+        });
