e7bde1f386586b0e00718121e3dc01862285251d
Doodflix <doodflixp@gmail.com>
2024-11-21 21:12:27 +0700

    Update piupiu.js

--- piupiu.js
+++ piupiu.js
@@ -1,5 +1,5 @@
         window.onload = function() {
-        var puURL = 'http://google.com';
+        var puURL = 'http://youtube.com';
         var puTS = Math.round(+new Date()/1000);
         console.log('T.'+localStorage.puTS+'/'+puTS);
         if (typeof localStorage.puTS == 'undefined' || parseInt(localStorage.puTS) <= (puTS - 5)) {
